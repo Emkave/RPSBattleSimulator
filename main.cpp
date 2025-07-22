118a6eb2a76a1f5a9de851d09d25370247e7cf72
Emkave <natik.agaev.emkave@gmail.com>
2025-07-22 20:11:20 +0300

    some changes

--- main.cpp
+++ main.cpp
@@ -2,30 +2,11 @@
 #include "vld.h"
 #include "object.h"
 #include <vector>
-
 #include "logic.h"
 #include "settings.h"
 
 using namespace sf;
 
-std::vector<object> spawn_objects() {
-    std::vector<object> objects;
-    objects.reserve(ROCK_AMOUNT + PAPER_AMOUNT + SCISSOR_AMOUNT);
-
-    for (size_t i=0; i<ROCK_AMOUNT; i++) {
-        objects.emplace_back(0);
-    }
-
-    for (size_t i=0; i<PAPER_AMOUNT; i++) {
-        objects.emplace_back(1);
-    }
-
-    for (size_t i=0; i<SCISSOR_AMOUNT; i++) {
-        objects.emplace_back(2);
-    }
-
-    return objects;
-}
 
 
 int main(void) {
@@ -34,30 +15,16 @@ int main(void) {
     RenderWindow window (VideoMode({WIN_LENGTH, WIN_HEIGHT}), "RPS", Style::Default, State::Windowed, settings);
     window.setFramerateLimit(60);
 
-    //object::init();
+    VertexArray lines(PrimitiveType::LineStrip, 5);
+    lines[0].position = Vector2f(20, 100);
+    lines[1].position = Vector2f(WIN_LENGTH-20, 100);
+    lines[2].position = Vector2f(WIN_LENGTH-20, WIN_HEIGHT-20);
+    lines[3].position = Vector2f(20, WIN_HEIGHT-20);
+    lines[4].position = Vector2f(20, 100);
 
     const Font font ("../assets/arial.ttf");
-    Text r_text (font, "");
-    Text p_text (font, "");
-    Text s_text (font, "");
-
-    r_text.setFillColor(Color::Cyan);
-    p_text.setFillColor(Color::Cyan);
-    s_text.setFillColor(Color::Cyan);
-
-    r_text.setPosition({0, 0});
-    p_text.setPosition({0, 30});
-    s_text.setPosition({0, 60});
-
-    std::vector<object> objects = spawn_objects();
-
-    Clock clock;
 
     while (window.isOpen()) {
-        r_text.setString("Rock amount: " + std::to_string(object::get_type_amount(0)));
-        p_text.setString("Paper amount: " + std::to_string(object::get_type_amount(1)));
-        s_text.setString("Scissor amount: " + std::to_string(object::get_type_amount(2)));
-
         while (const std::optional event = window.pollEvent()) {
             if (event->is<Event::Closed>()) {
                 window.close();
@@ -65,38 +32,7 @@ int main(void) {
         }
         window.clear(Color::Black);
 
-        const float dt = clock.restart().asSeconds();
-
-        for (size_t i=0; i<objects.size(); i++) {
-            objects[i].update(dt);
-
-            for (size_t j=i+1; j<objects.size(); j++) {
-                if (i != j && objects[i].get_type() != objects[j].get_type()) {
-                    auto & obj1 = objects[i];
-                    auto & obj2 = objects[j];
-
-                    const Vector2f delta = obj1.get_shape().getPosition() - obj2.get_shape().getPosition();
-                    const float dist2 = delta.x * delta.x + delta.y * delta.y;
-                    const float min_dist = obj1.get_shape().getRadius() * 2.f;
-
-                    if (dist2 < min_dist * min_dist) {
-                        const uint8_t winner = determine_winner(obj1.get_type(), obj2.get_type());
-
-                        if (winner == 1) {
-                            obj2.convert_to(obj1.get_type());
-                        } else if (winner == 2) {
-                            obj1.convert_to(obj2.get_type());
-                        }
-                    }
-                }
-            }
-
-            window.draw(objects[i].get_shape());
-        }
-
-        window.draw(r_text);
-        window.draw(p_text);
-        window.draw(s_text);
+        window.draw(lines);
 
         window.display();
     }
--- object.h
+++ object.h
@@ -3,7 +3,6 @@
 #include <stdexcept>
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
-#include <SFML/System/Clock.hpp>
 #include "settings.h"
 #include <random>
 
@@ -11,192 +10,74 @@
 using namespace sf;
 
 class object {
-private:
+protected:
     CircleShape shape;
-    uint8_t type = 0;
-    float dx = 0;
-    float dy = 0;
 
-    static size_t rock_amount;
-    static size_t scissor_amount;
-    static size_t paper_amount;
-
-    static Texture rock_texture;
-    static Texture scissor_texture;
-    static Texture paper_texture;
+private:
 
 public:
-    explicit object(const uint8_t && type) {
-        this->shape.setRadius(10.f);
-        this->shape.setOrigin({20.f, 20.f});
-        this->shape.setOutlineColor(Color::Green);
-
-
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-
-        switch (type) {
-            case 0:
-                object::rock_amount++;
-                this->shape.setFillColor(Color::Red);
-                {
-                    const std::uniform_real_distribution<float> dist_x(0.f, WIN_LENGTH - 1);
-                    const std::uniform_real_distribution<float> dist_y(110.f, WIN_HEIGHT - 1);
-                    this->shape.setPosition({dist_x(gen), dist_y(gen)});
-                }
-                this->type = 0;
-                break;
-
-            case 2:
-                object::scissor_amount++;
-                this->shape.setFillColor(Color::White);
-                {
-                    const std::uniform_real_distribution<float> dist_x(WIN_LENGTH / 4.f, 3.f * WIN_LENGTH / 4.f);
-                    const std::uniform_real_distribution<float> dist_y(110.f, WIN_HEIGHT / 4.f);
-                    shape.setPosition({dist_x(gen), dist_y(gen)});
-                }
-                this->type = 2;
-                break;
-
-            case 1:
-                object::paper_amount++;
-                {
-                    const std::uniform_real_distribution<float> dist_x(WIN_LENGTH / 2.f, WIN_LENGTH);
-                    const std::uniform_real_distribution<float> dist_y(110.f, WIN_HEIGHT);
-                    shape.setPosition({dist_x(gen), dist_y(gen)});
-                }
-                this->shape.setFillColor(Color::Yellow);
-                this->type = 1;
-                break;
-
-            default:
-                throw std::runtime_error("object::object() -> Invalid type given.\n");
-        }
-
-        constexpr float speed = 150.f;
-        const std::uniform_real_distribution<float> dist_angle(0.f, 2.f * 3.1415926f);
-        const float angle = dist_angle(gen);
-
-        this->dx = std::cos(angle) * speed;
-        this->dy = std::sin(angle) * speed;
-    }
-
-    inline CircleShape & get_shape() noexcept;
-    inline void update(const float dt);
-    inline uint8_t get_type() const noexcept { return type; }
-    inline void convert_to(uint8_t new_type);
-    static void init();
-    inline static size_t get_type_amount(const uint8_t && type) noexcept;
-
+    virtual ~object() = default;
+    virtual CircleShape get_shape(void) const noexcept;
+    virtual const uint8_t get_type(void) const noexcept;
 };
 
-size_t object::paper_amount = 0;
-size_t object::scissor_amount = 0;
-size_t object::rock_amount = 0;
 
-Texture object::rock_texture;
-Texture object::paper_texture;
-Texture object::scissor_texture;
+class rock final : public object {
+private:
+    static Texture texture;
+    static uint8_t type;
 
+public:
+    inline CircleShape get_shape(void) const noexcept override {return this->shape;}
+    inline const uint8_t get_type(void) const noexcept override {return rock::type;}
 
-inline CircleShape & object::get_shape() noexcept {
-    return this->shape;
-}
-
-
-inline void object::update(const float dt) {
-    auto pos = shape.getPosition();
-    pos.x += dx * dt;
-    pos.y += dy * dt;
-
-    if (pos.x - shape.getRadius() <= 0.f || pos.x + shape.getRadius() >= WIN_LENGTH) {
-        dx = -dx;
-    }
-
-    if (pos.y - shape.getRadius() <= 90.f || pos.y + shape.getRadius() >= WIN_HEIGHT) {
-        dy = -dy;
-    }
-
-    shape.setPosition(pos);
-}
-
-
-inline void object::convert_to(const uint8_t new_type) {
-    switch (this->type) {
-        case 0:
-            object::rock_amount--;
-            break;
-
-        case 1:
-            object::paper_amount--;
-            break;
-
-        case 2:
-            object::scissor_amount--;
-            break;
-        default: break;
-    }
-
-    this->type = new_type;
-
-    switch (this->type) {
-        case 0:
-            //shape.setTexture(&rock_texture);
-            this->shape.setFillColor(Color::Red);
-            object::rock_amount++;
-            break;
-        case 2:
-            //shape.setTexture(&scissor_texture);
-            this->shape.setFillColor(Color::White);
-            object::scissor_amount++;
-            break;
-        case 1:
-            //shape.setTexture(&paper_texture);
-            this->shape.setFillColor(Color::Yellow);
-            object::paper_amount++;
-            break;
-        default:
-            throw std::runtime_error("convert_to() -> Invalid type assigned!");
-    }
-}
-
-
-inline void object::init() {
-    if (rock_texture.getSize().x == 0) {
-        if (!rock_texture.loadFromFile(ROCK_PNG_PATH)) {
-            throw std::runtime_error("Failed to load rock texture");
+    static void init(void) {
+        if (!rock::texture.loadFromFile(ROCK_PNG_PATH)) {
+            throw std::runtime_error("rock::init() -> Invalid path.");
         }
-    }
 
-    if (scissor_texture.getSize().x == 0) {
-        if (!scissor_texture.loadFromFile(SCISSOR_PNG_PATH)) {
-            throw std::runtime_error("Failed to load scissor texture");
+        rock::type = 0;
+    }
+};
+
+
+class paper final : public object {
+private:
+    static Texture texture;
+    static uint8_t type;
+
+public:
+    inline CircleShape get_shape(void) const noexcept override {return this->shape;}
+    inline const uint8_t get_type(void) const noexcept override {return paper::type;}
+
+    static void init(void) {
+        if (!paper::texture.loadFromFile(PAPER_PNG_PATH)) {
+            throw std::runtime_error("paper::init() -> Invalid path.");
         }
-    }
 
-    if (paper_texture.getSize().x == 0) {
-        if (!paper_texture.loadFromFile(PAPER_PNG_PATH)) {
-            throw std::runtime_error("Failed to load paper texture");
+        paper::type = 1;
+    }
+};
+
+
+class scissors final : public object {
+private:
+    static Texture texture;
+    static uint8_t type;
+
+public:
+    inline CircleShape get_shape(void) const noexcept override {return this->shape;}
+    inline const uint8_t get_type(void) const noexcept override {return scissors::type;}
+
+    static void init(void) {
+        if (!scissors::texture.loadFromFile(SCISSOR_PNG_PATH)) {
+            throw std::runtime_error("scissors::init() -> Invalid path.");
         }
+
+        scissors::type = 2;
     }
-}
+};
 
-
-inline size_t object::get_type_amount(const uint8_t && type) noexcept {
-    switch (type) {
-        case 0:
-            return object::rock_amount;
-
-        case 1:
-            return object::paper_amount;
-
-        case 2:
-            return object::scissor_amount;
-
-        default:
-            return -1;
-    }
-}
 
 
 
